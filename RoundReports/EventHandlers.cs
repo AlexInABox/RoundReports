a7cfcedb279dfcf85a3ecf08ec668a042c7b379b
Alexander Betke <main@alexinabox.de>
2025-07-15 21:26:58 +0200

    chore: revert 8cd742c2fdc60ce82cb202368ea5c3429a940fe7

--- RoundReports/EventHandlers.cs
+++ RoundReports/EventHandlers.cs
@@ -285,8 +285,6 @@
             Points[PointTeam.Human] = new();
 
             MainPlugin.Reporter = new Reporter();
-            MainPlugin.Reporter.Kill();
-            MainPlugin.Reporter = new Reporter();
             MainPlugin.IsRestarting = false;
 
             // Map Generation Stats
